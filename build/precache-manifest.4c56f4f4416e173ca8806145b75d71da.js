1e76c0b0b0cd390f4b9bf2c984e6505240118162
pietroanello <pietro.anello@hotmail.it>
2022-10-14 11:51:21 +0200

    first commit

---
+++ build/precache-manifest.4c56f4f4416e173ca8806145b75d71da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d643eb6b94c89fd8e936ed669316a7e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae47bf20bc19268c89fc",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8cc97762567ea47b0e32",
+    "url": "./static/js/2.55cc3019.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.55cc3019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae47bf20bc19268c89fc",
+    "url": "./static/js/main.f139ffe2.chunk.js"
+  },
+  {
+    "revision": "85e73780a04f23f1adbd",
+    "url": "./static/js/runtime-main.ec0e4a84.js"
+  }
+]);
